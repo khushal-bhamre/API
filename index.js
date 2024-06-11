7e4785097489696db5c578270fa198168180333d
Khushal bhamre <bhamarek0@gmail.com>
2024-06-11 13:05:15 +0530

    implemented delete route

--- index.js
+++ index.js
@@ -84,12 +84,50 @@ app.patch('/api/users/:id',(req,res)=>{
         fs.writeFileSync(path.join(__dirname,'MOCK_DATA.json'),JSON.stringify(users));
 
         return res.json({success:'user successfully updated',user:updatedUser})
-        
+
      } catch (error) {
         return res.status(505).json({error:'failed to update the user'})
      }
 })
 
+//delete route
+
+app.delete('/api/users/:id',(req,res)=>{
+
+    //find the user to delete
+    const id = req.params.id;
+
+    //find the index of user which is been req
+
+    const userIndex =users.findIndex((user)=> user.id == id);
+
+    const deletedUser = users[userIndex];
+
+    if(userIndex == -1){
+        return res.status(404).json({error:'user not found'});
+     }
+
+     //remove the user 
+     users.splice(userIndex, 1);
+
+     //update the further id 
+
+     for (let index = userIndex; index < users.length; index++) {
+        users[index].id--; 
+     }
+
+     //sync
+
+     try {
+        fs.writeFileSync(path.join(__dirname,'MOCK_DATA.json'),JSON.stringify(users));
+
+        return res.json({success:'user successfully deleted',user:deletedUser})
+
+     } catch (error) {
+        return res.status(505).json({error:'failed to delete the user'})
+     }
+     
+})
 
 
 app.listen(port,()=>{
